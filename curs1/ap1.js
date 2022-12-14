0d46cc7d6a7d5a4779708aa35af25d2e05cdc422
sergiuradu <sr.serj@yahoo.com>
2022-12-14 01:58:09 +0100

    update

---
+++ curs1/ap1.js
@@ -0,0 +1,12 @@
+
+alert('Salut')
+
+window.alert('Salut JavaScript')
+
+confirm('Vreau sa invat JavaScript')
+
+console.log('Mesaj in consola')
+
+console.info('Info Status')
+
+console.error('Mesaj de eroare!')
